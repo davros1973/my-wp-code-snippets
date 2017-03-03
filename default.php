b704acae5e5a63f5fa8df7a1c01d486fbc9f0794
David Bayliss <gitHub@xarta.co.uk>
2017-03-03 19:09:57 +0000

    Update default.php

--- default.php
+++ default.php
@@ -1,1 +1,9 @@
-// This is the default file returned by xgithub shortcode (my-wp-code-snippet) if the "raw" attribute is not set.
+/* This is the default file returned by xgithub shortcode (my-wp-code-snippet) if the "raw" attribute is not set.
+
+   EXAMPLE USAGE:
+
+   [xgithub raw="borg-contact/master/contactpagefooter.js" lang="js" ]
+
+   Has dependencies:  see https://blog.xarta.co.uk
+*/
+
