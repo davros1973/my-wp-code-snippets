384e7a503ec30c7c9a3d93560752f4d63efb72d2
David Bayliss <gitHub@xarta.co.uk>
2017-03-04 01:52:10 +0000

    xgithub.php Prevent unwanted shortcode evaluation
    i.e. if this code snippet source were fed to $github (in the function) then any WordPress shortcodes in the source would actually be evaluated.  So temporarily change square brackets to entities, and then change back again once danger is over.

--- xgithub.php
+++ xgithub.php
@@ -5,18 +5,35 @@ function xgithub_shortcode( $atts )
 	
     extract( shortcode_atts( array(
 		'raw' => 'my-wp-code-snippets/master/default.php',
-	    	'lang' => 'code',
-		'light' => '1'
+	    'lang' => 'code',
+	    'light' => '1'
 	), $atts ) );
 	$raw = "https://raw.githubusercontent.com/davros1973/$raw";
 	$colorboxID = trim(strval(uniqid()));
   
-	$github = do_shortcode(" [github raw=$raw] ");
-	$syntax = apply_filters('the_content', "[$lang title=$raw classname=\"BigCode\" light=\"1\"] $github [/$lang]" );
+	$github = x_squarebrackets_to_htmlentities(do_shortcode(" [github raw=$raw] "));
+	$syntax = apply_filters('the_content', "[$lang title=$raw classname=\"BigCode\" light=$light ] $github [/$lang]" );
 	$wrap = '<div class="MyCodeStyle MyCodeWidth MyCodeButtons"><div id="wp_colorbox_'.$colorboxID.'">'.$syntax.'</div>&nbsp;</div>';
 	$start = '<p style="clear:both;">...</p><p><span style="float:right;"> ';
 	$colorbox = do_shortcode($start . ' [wp_colorbox_media url="#wp_colorbox_'.$colorboxID.'" type="inline" hyperlink="" alt="CODE ZOOM"] ' . "</span><p>$wrap");
   
- 	return $colorbox;
+ 	return x_htmlentities_to_squarebrackets($colorbox);
 }
 add_shortcode('xgithub', 'xgithub_shortcode');
+
+// square bracket functions to prevent other shortcodes in the source file from being evaluated!
+function x_squarebrackets_to_htmlentities( $input)
+{
+ 	 $output = str_replace('[','&#091;', $input);
+     $output = str_replace(']','&#093;', $output);
+						   
+	return $output;
+}
+
+function x_htmlentities_to_squarebrackets( $input)
+{
+ 	 $output = str_replace('&amp;#091;','[', $input);
+     $output = str_replace('&amp;#093;',']', $output);
+						   
+	return $output;
+}
